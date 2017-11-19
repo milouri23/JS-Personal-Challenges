8af6ce244b09d565b3efbe5031997e655658932f
Camilo Uribe Díaz <milouri23@gmail.com>
2017-11-18 19:55:22 -0500

    Add function definition - Challenge #1 solved

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function netIPcalc(hostIP, netmask) {
-    
+    hostIP = hostIP.split( "." ).map( Number );
+    netmask = netmask.split( "." ).map( Number );
+    netIP = [];
+
+    for (let index = 0; index < hostIP.length; index++) {
+        netIP.push( hostIP[index] & netmask[index] + 256);
+    }
+
+    return netIP.join(".");
 }
 const hostIP = "172.16.10.4";
 const netmask = "255.255.0.0";
-console.log(netIPcalc(hostIP, netmask));
+console.log( netIPcalc(hostIP, netmask) );
